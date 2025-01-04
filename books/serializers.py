47ac6f42658df4c83ffbffed2c25e809e70708da
Freddy Alexander <55334620+br2bb7t@users.noreply.github.com>
2025-01-03 21:54:50 -0500

    Update serializers.py

--- books/serializers.py
+++ books/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 class BookSerializer(serializers.Serializer):
+    _id = serializers.CharField()
     title = serializers.CharField(max_length=255)
     author = serializers.CharField(max_length=255)
     published_date = serializers.DateField()
